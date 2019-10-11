0ec744133684781e0cdff3a39e00d07353dcbc3c
meltemsungurlu <56429602+meltemsungurlu@users.noreply.github.com>
2019-10-11 16:00:07 +0300

    Edit

--- sample/popup.js
+++ sample/popup.js
@@ -1,7 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+// https://developer.chrome.com/extensions/getstarted
 'use strict';
 
 let changeColor = document.getElementById('changeColor');
@@ -10,10 +10,12 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
- 
+
 changeColor.onclick = function(element) {
   let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query(
+    {active: true, currentWindow: true},
+    function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
