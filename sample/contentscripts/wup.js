805be7ad4a22a391d608cf2221e4c6375dfa59d7
Cüneyt Ayanoğlu <cayanoglu@gmail.com>
2020-05-05 04:43:34 +0300

    Iterate

--- sample/contentscripts/wup.js
+++ sample/contentscripts/wup.js
@@ -191,18 +191,7 @@ let parseContactInfo = function() {
 
 }
 
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-}
+let download=ayanoglu.utility.download;
 
 if (typeof window.contacts === 'undefined')
     window.contacts = {};
